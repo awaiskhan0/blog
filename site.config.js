074c4e0c243fa8c422786e8392f163cd52d97fc1
Awais Khan <awaisk@hotmail.co.uk>
2023-04-17 02:47:54 +0100

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   blog: {
     title: "Blog",
     description: "Awais Khan - Blog",
-    theme: "dark", // ['light', 'dark', 'auto']
+    theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
